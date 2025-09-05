4e1275a2de690786e98d5728bbff8d6ffb989a43
Blocks <steve.smith@frenchn1270.site>
2025-09-05 11:44:44 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+window.bmTestCommand = function(args) {
+    const timestamp = new Date().toISOString().replace('T', ' ').split('.')[0];
+    if (args[0] === 'test') {
+        return `bm:test [${timestamp}] hello world`;
+    } else {
+        return `bm:test [${timestamp}] Unknown command. Usage: bm test`;
+    }
+};
